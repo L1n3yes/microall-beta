22e8b4a7cf8049886ad707bbb201bd407b06da26
L1n3yes <140054386+L1n3yes@users.noreply.github.com>
2023-10-27 17:52:58 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,62 +1,13 @@
-let dice = 0
-function NOUSE () {
-    // this version will only show beta stuff and NOT STUFF FROM MICRO:ALL
-    basic.showString("read comment")
-}
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
 })
+input.onButtonPressed(Button.B, function () {
+    sprite.move(1)
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 5)
 basic.forever(function () {
-    dice = randint(1, 6)
-    if (dice == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (dice == 2) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # . # .
-            . . . . .
-            . . . . .
-            `)
-    } else if (dice == 3) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # . # . #
-            . . . . .
-            . . . . .
-            `)
-    } else if (dice == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-    } else if (dice == 5) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
-    } else if (dice == 6) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            `)
-    } else {
-    	
+    if (sprite.get(LedSpriteProperty.X) == 4) {
+        sprite.set(LedSpriteProperty.X, 0)
     }
 })
